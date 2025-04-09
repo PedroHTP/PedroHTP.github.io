03a457f44d563492e5068ec3a4b10dff9ede41fa
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-04-09 13:44:56 -0300

    Deletando projetos

--- projetos/atv13 - CRUD (sequencial)/includes/message.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-        if (isset($_SESSION['mensagem'])) {
-            echo "<script>window.onload = function () {M.toast({html: '".$_SESSION['mensagem']."'});};</script>";
-        }
-    session_unset();
